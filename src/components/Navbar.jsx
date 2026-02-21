508af6afa4e94ff39140feaea15159a294afa179
wordpress-developer01 <130246057+wordpress-developer01@users.noreply.github.com>
2026-02-21 16:02:50 +0300

    started setting Sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,8 @@ import FlexBetween from './FlexBetween';
 import { useDispatch } from 'react-redux';
 import { setMode } from 'states';
 import profileImage from "assets/avatar-15.svg";
-import { AppBar, useTheme, Toolbar } from '@mui/material';
+import { AppBar, useTheme, Toolbar, InputBase, IconButton } from '@mui/material';
+
 
 const Navbar = () => {
     const dispatch = useDispatch();
@@ -18,7 +19,34 @@ const Navbar = () => {
     }}
     >
         <Toolbar sx={{ justifyContent: "space-between"}}>
-
+         <FlexBetween>
+             <IconButton onClick={() => console.log('open/close sidebar')}>
+            <MenuIcon/>
+             </IconButton>
+          <FlexBetween backgroundColor={theme.palette.background.alt}
+          borderRadius='9px'
+          gap='3rem'
+          p={'0.1rem 1.5rem'}
+          >
+         
+            <InputBase placeholder='Search...'/>
+            <IconButton>
+              <Search/>
+            </IconButton>
+          </FlexBetween>
+         </FlexBetween>  
+         {/* {Right side} */}
+         <FlexBetween gap={'1.5rem'}>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === 'dark' ? (
+              <DarkModeOutlined sx={{ fontSize: '25px'}}/>
+            ) : <LightModeOutlined sx={{ fontSize: '25px'}}/>
+          }
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: '25px'}}/>
+          </IconButton>
+         </FlexBetween>
         </Toolbar>
     </AppBar>
   )
--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -3,12 +3,16 @@ import { useSelector } from 'react-redux';
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from 'components/Navbar';
+import Sidebar from 'components/Sidebar';
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery('(min-width): 600px')
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
-    <Box width='100%' height='100%'>
+    <Box display={isNonMobile ? 'flex' : 'block'} width='100%' height='100%'>
+      <Sidebar isNonMobile={isNonMobile} drawerWidth='250px' isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
      <Box>
-     <Navbar/>
+     <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/>
      <Outlet/>
      </Box>
     </Box>
